0b9ccdd5d7b723ce3f80d7d24af433130cabb0dc
nayonsoso <panda0329@naver.com>
2023-11-14 01:45:30 +0900

    feat: 혜택 내역 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,11 +5,19 @@ import java.util.EnumMap;
 
 public class OutputView {
     private static final String EVENT_PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+
     private static final String MENU_START_MESSAGE = "<주문 메뉴>";
     private static final String MENU_QUANTITY = "%s %d개";
+
     private static final String PRICE_BEFORE_DISCOUNT_MESSAGE = "<할인 전 총주문 금액>";
+
     private static final String GIFT_MENU_MESSAGE = "<증정 메뉴>";
+    private static final String GIFT_MENU = "샴페인";
+    private static final int GIFT_MENU_QUANTITY = 1;
+
     private static final String BENEFIT_LIST_MESSAGE = "<혜택 내역>";
+    private static final String BENEFIT = "%s: -%s";
+
     private static final String TOTAL_BENEFIT_AMOUNT_MESSAGE = "<총혜택 금액>";
     private static final String PRICE_AFTER_DISCOUNT_MESSAGE = "<할인 후 예상 결제 금액>";
     private static final String EVENT_BADGE_MESSAGE = "<12월 이벤트 배지>";
@@ -36,6 +44,12 @@ public class OutputView {
         System.out.println(GIFT_MENU_MESSAGE);
         System.out.printf(MENU_QUANTITY, GIFT_MENU, GIFT_MENU_QUANTITY);
     }
+
+    public void printBenefitList(String event, int discount){
+        System.out.println(BENEFIT_LIST_MESSAGE);
+        System.out.printf(BENEFIT, event, formatWithCommas(discount));
+    }
+
 
     private void printMoney(int money){
         System.out.println(formatWithCommas(money));
