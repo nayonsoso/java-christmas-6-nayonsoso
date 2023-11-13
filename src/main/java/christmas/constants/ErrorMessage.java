ea62e7195591c38757e9d06b150a890f56cbde3e
nayonsoso <panda0329@naver.com>
2023-11-14 02:15:03 +0900

    feat: 날짜에 숫자가 아닌 값 입력시 에러 처리 구현

---
+++ src/main/java/christmas/constants/ErrorMessage.java
@@ -0,0 +1,6 @@
+package christmas.constants;
+
+public class ErrorMessage {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String INVALID_DATE_ERROR = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,19 +2,31 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static christmas.constants.ErrorMessage.INVALID_DATE_ERROR;
+
 public class InputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String REQUEST_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String REQUEST_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-    public static String readDate() {
+    public static int readDate() {
         System.out.println(START_MESSAGE);
         System.out.println(REQUEST_DATE);
-        return Console.readLine();
+        String inputDate = Console.readLine();
+        validateDate(inputDate);
+        return Integer.parseInt(inputDate);
     }
 
     public static String readMenu() {
         System.out.print(REQUEST_MENU);
         return Console.readLine();
+    }
+
+    private static void validateDate(String inputDate) {
+        try {
+            Integer.parseInt(inputDate);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(INVALID_DATE_ERROR);
+        }
     }
 }
