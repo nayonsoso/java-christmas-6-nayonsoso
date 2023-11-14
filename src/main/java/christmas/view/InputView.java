c11dc4eda77c29e5bd5dd6f89f572ff1d88395cb
nayonsoso <panda0329@naver.com>
2023-11-14 19:37:09 +0900

    refactor: 출력 형식 줄바꿈 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,22 +5,20 @@ import christmas.constants.Constants;
 import christmas.constants.ErrorMessage;
 
 public class InputView {
-    private static final String START_MESSAGE = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.";
-    private static final String REQUEST_DATE = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String START_MESSAGE = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.\n";
+    private static final String REQUEST_DATE = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n";
     private static final String REQUEST_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public static int readDate() {
         System.out.printf(START_MESSAGE, Constants.MONTH);
-        System.out.println();
         System.out.printf(REQUEST_DATE, Constants.MONTH);
-        System.out.println();
         String inputDate = Console.readLine();
         validateDate(inputDate);
         return Integer.parseInt(inputDate);
     }
 
     public static String readOrder() {
-        System.out.print(REQUEST_MENU);
+        System.out.println(REQUEST_MENU);
         return Console.readLine();
     }
 
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,13 +8,13 @@ import java.text.DecimalFormat;
 public class OutputView {
     private static final String EVENT_PREVIEW_MESSAGE = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String MENU_START_MESSAGE = "<주문 메뉴>";
-    private static final String MENU_QUANTITY = "%s %d개";
+    private static final String MENU_QUANTITY_FORMAT = "%s %d개";
     private static final String PRICE_BEFORE_DISCOUNT_MESSAGE = "<할인 전 총주문 금액>";
     private static final String GIFT_MENU_MESSAGE = "<증정 메뉴>";
     private static final String GIFT_MENU = "샴페인";
     private static final int GIFT_MENU_QUANTITY = 1;
     private static final String BENEFIT_LIST_MESSAGE = "<혜택 내역>";
-    private static final String BENEFIT = "%s: %s";
+    private static final String BENEFIT_FORMAT = "%s: %s";
     private static final String TOTAL_BENEFIT_AMOUNT_MESSAGE = "<총혜택 금액>";
     private static final String PRICE_AFTER_DISCOUNT_MESSAGE = "<할인 후 예상 결제 금액>";
     private static final String EVENT_BADGE_MESSAGE = "<%d월 이벤트 배지>";
@@ -30,7 +30,7 @@ public class OutputView {
     }
 
     public static void printMenu(String menu, Integer quantity) {
-        System.out.printf(MENU_QUANTITY, menu, quantity);
+        System.out.printf(MENU_QUANTITY_FORMAT, menu, quantity);
         System.out.println();
     }
 
@@ -43,12 +43,12 @@ public class OutputView {
     public static void printGiftMenu() {
         System.out.println();
         System.out.println(GIFT_MENU_MESSAGE);
-        System.out.printf(MENU_QUANTITY, GIFT_MENU, GIFT_MENU_QUANTITY);
+        System.out.printf(MENU_QUANTITY_FORMAT, GIFT_MENU, GIFT_MENU_QUANTITY);
     }
 
     public static void printBenefitList(String event, int discount) {
         System.out.println(BENEFIT_LIST_MESSAGE);
-        System.out.printf(BENEFIT, event, formatMinusWithCommas(discount));
+        System.out.printf(BENEFIT_FORMAT, event, formatMinusWithCommas(discount));
     }
 
     public static void printTotalBenefit(int totalBenefit) {
@@ -67,7 +67,7 @@ public class OutputView {
     }
 
     public static void printError(String message){
-        System.out.println(ErrorMessage.ERROR_PREFIX + message);
+        System.out.printf("%s %s\n\n", ErrorMessage.ERROR_PREFIX , message);
     }
 
     private static String formatWithCommas(int money) {
