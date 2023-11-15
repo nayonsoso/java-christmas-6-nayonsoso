254fe02dd55003a032e329fab7374783d5df3f7e
nayonsoso <panda0329@naver.com>
2023-11-15 10:24:20 +0900

    refactor: 에러메세지 prefix 공백 수정

--- src/main/java/christmas/constants/ErrorMessage.java
+++ src/main/java/christmas/constants/ErrorMessage.java
@@ -1,7 +1,7 @@
 package christmas.constants;
 
 public class ErrorMessage {
-    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_PREFIX = "[ERROR]";
     public static final String INVALID_DATE_ERROR = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public static final String INVALID_ORDER_ERROR = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
 }
