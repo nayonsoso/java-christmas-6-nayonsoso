5b6ef2cd8d3c3c29a24cb2abaaac7fd7ae2ae643
nayonsoso <panda0329@naver.com>
2023-11-15 10:25:21 +0900

    refactor: "없음" 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,23 +7,29 @@ import java.text.DecimalFormat;
 
 public class OutputView {
     private static final String EVENT_PREVIEW_MESSAGE = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
-    private static final String MENU_START_MESSAGE = "\n<주문 메뉴>";
-    private static final String MENU_QUANTITY_FORMAT = "%s %d개\n";
-    private static final String PRICE_BEFORE_DISCOUNT_MESSAGE = "\n<할인 전 총주문 금액>";
-    private static final String GIFT_MENU_MESSAGE = "\n<증정 메뉴>";
-    private static final String GIFT_MENU = "샴페인";
-    private static final int GIFT_MENU_QUANTITY = 1;
-    private static final String BENEFIT_LIST_MESSAGE = "<혜택 내역>";
-    private static final String BENEFIT_FORMAT = "%s: %s";
-    private static final String TOTAL_BENEFIT_AMOUNT_MESSAGE = "<총혜택 금액>";
-    private static final String PRICE_AFTER_DISCOUNT_MESSAGE = "<할인 후 예상 결제 금액>";
-    private static final String EVENT_BADGE_MESSAGE = "<%d월 이벤트 배지>";
 
-    public static void printEventPreview(int date) {
-        System.out.printf(EVENT_PREVIEW_MESSAGE, Constants.MONTH, date);
+    private static final String MENU_START_MESSAGE = "\n<주문 메뉴>";
+    private static final String GIFT_MENU_MESSAGE = "\n<증정 메뉴>";
+    private static final String MENU_QUANTITY_FORMAT = "%s %d개\n";
+
+    private static final String PRICE_BEFORE_DISCOUNT_MESSAGE = "\n<할인 전 총주문 금액>";
+    private static final String PRICE_AFTER_DISCOUNT_MESSAGE = "\n<할인 후 예상 결제 금액>";
+    private static final String PLUS_MONEY_FORMAT = "###,###원";
+
+    private static final String BENEFIT_LIST_MESSAGE = "\n<혜택 내역>";
+    private static final String BENEFIT_FORMAT = "%s: %s\n";
+
+    private static final String TOTAL_BENEFIT_AMOUNT_MESSAGE = "\n<총혜택 금액>";
+    private static final String MINUS_MONEY_FORMAT = "-###,###원";
+
+    private static final String EVENT_BADGE_MESSAGE = "\n<%d월 이벤트 배지>\n";
+    private static final String NO_DATA = "없음";
+
+    public static void printEventPreview(int month, int date) {
+        System.out.printf(EVENT_PREVIEW_MESSAGE, month, date);
     }
 
-    public static void printMenuStart() {
+    public static void printMenuStartMessage() {
         System.out.println(MENU_START_MESSAGE);
     }
 
@@ -36,19 +42,31 @@ public class OutputView {
         System.out.println(formatWithCommas(price));
     }
 
-    public static void printGiftMenu() {
-        System.out.println();
+    public static void printGiftMenuStartMessage() {
         System.out.println(GIFT_MENU_MESSAGE);
-        System.out.printf(MENU_QUANTITY_FORMAT, GIFT_MENU, GIFT_MENU_QUANTITY);
+    }
+
+    public static void printGiftMenu(String giftMenu, int quantity) {
+        System.out.printf(MENU_QUANTITY_FORMAT, giftMenu, quantity);
+    }
+
+    public static void printNoDataMessage() {
+        System.out.println(NO_DATA);
+    }
+
+    public static void printBenefitListStartMessage() {
+        System.out.println(BENEFIT_LIST_MESSAGE);
     }
 
     public static void printBenefitList(String event, int discount) {
-        System.out.println(BENEFIT_LIST_MESSAGE);
         System.out.printf(BENEFIT_FORMAT, event, formatMinusWithCommas(discount));
     }
 
-    public static void printTotalBenefit(int totalBenefit) {
+    public static void printTotalBenefitStartMessage() {
         System.out.println(TOTAL_BENEFIT_AMOUNT_MESSAGE);
+    }
+
+    public static void printTotalBenefit(int totalBenefit) {
         System.out.println(formatMinusWithCommas(totalBenefit));
     }
 
@@ -57,22 +75,28 @@ public class OutputView {
         System.out.println(formatWithCommas(priceAfterDiscount));
     }
 
-    public static void printEventBadge(String badge) {
+    public static void printEventBadgeStartMessage() {
         System.out.printf(EVENT_BADGE_MESSAGE, Constants.MONTH);
+    }
+
+    public static void printEventBadge(String badge) {
         System.out.println(badge);
     }
 
-    public static void printError(String message){
-        System.out.printf("%s %s\n\n", ErrorMessage.ERROR_PREFIX , message);
+    public static void printError(String message) {
+        System.out.printf("%s %s\n\n", ErrorMessage.ERROR_PREFIX, message);
     }
 
     private static String formatWithCommas(int money) {
-        DecimalFormat df = new DecimalFormat("###,###원");
+        DecimalFormat df = new DecimalFormat(PLUS_MONEY_FORMAT);
         return df.format(money);
     }
 
     private static String formatMinusWithCommas(int money) {
-        DecimalFormat df = new DecimalFormat("-###,###원");
+        if (money == 0) {
+            return formatWithCommas(money);
+        }
+        DecimalFormat df = new DecimalFormat(MINUS_MONEY_FORMAT);
         return df.format(money);
     }
 }
